c58bca5fdcd34aba812e2bf07de5a5cab845c0ee
Matt Stover <mcs@mcstover.com>
2017-07-27 18:25:58 -0700

    Initial setup with Vue.js and webpack. Also includes compiled kiva styleguide css.

---
+++ README.md
@@ -0,0 +1,27 @@
+# kiva-borrower-map
+
+> Presentation of Kiva Borrowers businesses on a map. Uses a combintation of known business entities with a Yelp link along with the Yelp api to gather accurate location data to power the map. 
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app" class="pageWrap">
+    <div class="xbLegacyNav">
+      <div class="top-nav" role="navigation">
+        <div class="row header-row">
+          <div class="small-3 large-2 xxlarge-2 columns header-column logo-area">
+            <a class="header-button" href="https://www.qa.kiva.org/">
+              <svg class="icon icon-new-kiva-logo">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-new-kiva-logo"></use>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+}
+</style>
