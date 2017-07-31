1479a1822d612b6c3f80f4dd61cdf4f93665b363
Matt Stover <mcs@mcstover.com>
2017-07-31 00:15:08 -0700

    Added ajax call to retrieve locations from node locations api. Tweaked home page with temporary map just a bit.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,37 +1,53 @@
 <template>
   <div class="container">
     <h1>{{ msg }}</h1>
+    <div class="list-holder">
+      <LocationList v-bind:fetchedLocations="locations"></LocationList>
+    </div>
   </div>
 </template>
 
 <script>
+import LocationList from './Location-List.vue'
+import * as axios from 'axios'
+
 export default {
   name: 'home',
   data () {
     return {
-      msg: 'Kiva Borrowers Across the United States'
+      msg: 'Kiva US Borrowers',
+      locations: []
     }
+  },
+  components: {
+    LocationList
+  },
+  methods: {
+    fetchLocations: function (query) {
+      let _this = this
+      console.log(query)
+      axios.get('/locations').then((data) => {
+        console.log(data)
+        data.data.forEach(function (element) {
+          _this.locations.push(element)
+        }, this)
+      })
+    }
+  },
+  mounted () {
+    console.log('Home is mounted')
+    this.fetchLocations('test query')
+  },
+  beforeCreate () {
+    console.log('BEFORE Home is created')
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-} */
+  h1 {
+    margin: 0;
+    padding: 1rem .5rem;
+  }
 </style>
