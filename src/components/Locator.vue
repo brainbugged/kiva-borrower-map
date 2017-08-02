427a741b20465d5109605bc3f4e511ccf933a9e7
m.fab <go-bears@users.noreply.github.com>
2017-08-02 12:19:20 -0700

    draft of geolocator button

---
+++ src/components/Locator.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id='locator'></div>
+</template>
+
+<script>
+export default {
+  name: 'userLocate',
+  // is this how to register button to appear in the #locator?
+  template: '<button onclick="getLocation()">i am here</button>',
+  data () {
+    return {
+      // userLocate: {}
+    }
+  },
+  methods: {
+    // why is compiler saying 'let is a reserved word?' when I'm trying to make it a variable?
+    let locatorDiv = document.getElementById('locator')
+
+    // trying to translate html5 geolocation code to vue??s
+    getLocation: function () {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+      } else {
+        locatorDiv.innerHTML = 'Geolocation is not supported by this browser.'
+      }
+    },
+    showPosition: function (position) {
+      locatorDiv.innerHTML = 'Latitude: ' + position.coords.latitude +
+        '<br>Longitude: ' + position.coords.longitude
+
+        // returns userLocation so it can be used the center the map?
+        let userLocation = {'lat': position.coords.latitude,
+          'lng': position.coords.longitude
+        }
+        return userLocation
+      }
+    }
+  }
+
+</script>
