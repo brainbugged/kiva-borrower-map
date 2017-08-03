51cdcc0437713b37fde969a401a01ab8b4320271
m.fab <go-bears@users.noreply.github.com>
2017-08-02 17:58:32 -0700

    remove comments

--- src/components/Locator.vue
+++ src/components/Locator.vue
@@ -17,8 +17,6 @@ export default {
     locatorDiv: function () {
       return document.getElementById('userLocation')
     },
-    // locatorDiv: document.getElementById('userLocation'),
-    // trying to translate html5 geolocation code to vue??s
     getLocation: function () {
       if (window.navigator.geolocation) {
         window.navigator.geolocation.getCurrentPosition(this.showPosition)
@@ -31,12 +29,6 @@ export default {
         '<br>Longitude: ' + position.coords.longitude
 
       this.$emit('getLocation', position.coords)
-      // returns userLocation so it can be used the center the map?
-      // console.log(position.coords)
-      // let userLocation = {'lat': position.coords.latitude,
-      //   'lng': position.coords.longitude
-      // }
-      // return userLocation
     }
   }
 }
